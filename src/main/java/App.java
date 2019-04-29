5e9c39fbf3e26e509bc0ad637f7827a057a900b3
j0hnton <afrikiddo@gmail.com>
2019-04-29 17:19:02 +0300

    Add sighting database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -52,11 +52,42 @@ public class App {
             model.put("track", request.session().attribute("track"));
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
         get("/endangereddisplay", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "public/template/endangereddisplay.vtl");
             model.put("track", request.session().attribute("track"));
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        //post//
+
+        post("/animaldisplay", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String rangername = request.queryParams("rangername");
+            String animalname = request.queryParams("animalname");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            String location = request.queryParams("location");
+
+            animal newAnimal = new animal(rangername, animalname, health, age, location);
+            newAnimal.save();
+            model.put("template", "public/template/animaldisplay.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        post("/endangereddisplay", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String rangername = request.queryParams("rname");
+            String animalname = request.queryParams("aname");
+            String health = request.queryParams("ahealth");
+            String age = request.queryParams("aage");
+            String location = request.queryParams("alocation");
+
+            sighting newSighting = new sighting (rangername, animalname, health, age, location);
+            newSighting.save();
+            model.put("template", "public/template/endangereddisplay.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
